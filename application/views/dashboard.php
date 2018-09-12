9095d8c026796efc72ae3281d32c8fdcbc48a58d
Moon Lee <lee.moonkyu@gmail.com>
2018-09-12 10:53:56 +1000

    tidy up files

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,3 +1,1 @@
-<!-- <button type="button" name="" id="event_load" class="btn" btn-lg btn-block"> ajex text</button> -->
-<!-- <a class="btn" href="<?php echo base_url('dashboard/eventslist');?>" role="button">get calendar json data </a> -->
 <div id='calendar'></div>
