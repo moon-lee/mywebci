9e0a8bbb4f119b8e65a76e0bef6ae3d4c8910d1f
Moon Lee <lee.moonkyu@gmail.com>
2018-11-09 01:19:14 +1000

    add inputs

--- application/views/settings.php
+++ application/views/settings.php
@@ -5,14 +5,50 @@
                 <i class="fas fa-cog"></i><strong>{contenttitle}</strong>
             </div>
         </div>
-    
-        <div class="card my-4">
+
+        <div class="card-settings card my-4">
             <div class="card-body">
-                <div class="color">
-                    <p>Setting Page</p>
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <form action="" method="post">
+                            <p><span class="far fa-plus-square"></span>General</p>
+                            <div class="form-group">
+                                <label for="paymentDate">General Setting1</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control">
+                                    <div class="invalid-tooltip"></div>
+                                </div>
+                            </div>
+                            <p><span class="far fa-plus-square"></span>Payments</p>
+                            <div class="form-group">
+                                <label for="paymentDate">Payments Setting1</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control">
+                                    <div class="invalid-tooltip"></div>
+                                </div>
+                            </div>
+                            <p><span class="far fa-plus-square"></span>Spendings</p>
+                            <div class="form-group">
+                                <label for="paymentDate">Spendings Setting1</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control">
+                                    <div class="invalid-tooltip"></div>
+                                </div>
+                            </div>
+                            <p><span class="far fa-plus-square"></span>Weights</p>
+                            <div class="form-group">
+                                <label for="paymentDate">Weights Setting1</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control">
+                                    <div class="invalid-tooltip"></div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-        
+
     </div>
 </div>
